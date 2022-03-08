746c6c5478282bba753488a9eaa6fa0e65cb1e06
eduardoguiavet <95000172+eduardoguiavet@users.noreply.github.com>
2022-03-08 14:41:36 -0300

    refactory

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,14 +36,7 @@ EXIT_CODE=$3
 
 # Read TF_WORKSPACE environment variable or use "default"
 WORKSPACE=${TF_WORKSPACE:-default}
-echo " $WORKSPACE $WORKTFDIR "
 WORKTFDIR=${WORKTFDIR}
-
-echo " AQUI AQUI AQUI AQUI AQUI AQUI AQUI AQUI AQUI AQUI AUQI"
-echo " $GITHUB_EVENT_PATH" "
-echo " AQUI AQUI AQUI AQUI AQUI AQUI AQUI AQUI AQUI AQUI AUQI"
-echo " $WORKSPACE $WORKTFDIR "
-
 # Read EXPAND_SUMMARY_DETAILS environment variable or use "true"
 if [[ ${EXPAND_SUMMARY_DETAILS:-true} == "true" ]]; then
   DETAILS_STATE=" open"
@@ -177,7 +170,7 @@ fi
 if [[ $COMMAND == 'plan' ]]; then
   # Look for an existing plan PR comment and delete
   echo -e "\033[34;1mINFO:\033[0m Looking for an existing plan PR comment."
-  PR_COMMENT_ID=$(curl -sS -H "$AUTH_HEADER" -H "$ACCEPT_HEADER" -L "$PR_COMMENTS_URL" | jq '.[] | select(.body|test ("#### Terraform `plan` .* for Workspace: `'"$WORKSPACE"'`/`'"$WORKTFDIR"'`")) | .id')
+  PR_COMMENT_ID=$(curl -sS -H "$AUTH_HEADER" -H "$ACCEPT_HEADER" -L "$PR_COMMENTS_URL" | jq '.[] | select(.body|test ("### Terraform `plan` .* for Workspace: `'"$WORKSPACE"'` `'"$WORKTFDIR"'`")) | .id')
   if [ "$PR_COMMENT_ID" ]; then
     echo -e "\033[34;1mINFO:\033[0m Found existing plan PR comment: $PR_COMMENT_ID. Deleting."
     PR_COMMENT_URL="$PR_COMMENT_URI/$PR_COMMENT_ID"
@@ -197,7 +190,7 @@ if [[ $COMMAND == 'plan' ]]; then
     if [[ $COLOURISE == 'true' ]]; then
       CLEAN_PLAN=$(echo "$CLEAN_PLAN" | sed -r 's/^~/!/g') # Replace ~ with ! to colourise the diff in GitHub comments
     fi
-    PR_COMMENT="#### Terraform \`plan\` Succeeded for Workspace: \`$WORKSPACE\`/ \`$WORKTFDIR\`
+    PR_COMMENT="### Terraform \`plan\` Succeeded for Workspace: \`$WORKSPACE\` \`$WORKTFDIR\`
 <details$DETAILS_STATE><summary>Show Output</summary>
 
 \`\`\`diff
@@ -210,7 +203,7 @@ $CLEAN_PLAN
   # Meaning: Terraform plan failed.
   # Actions: Build PR comment.
   if [[ $EXIT_CODE -eq 1 ]]; then
-    PR_COMMENT="#### Terraform \`plan\` Failed for Workspace: \`$WORKSPACE\`/\`$WORKTFDIR\`
+    PR_COMMENT="### Terraform \`plan\` Failed for Workspace: \`$WORKSPACE\` \`$WORKTFDIR\`
 <details$DETAILS_STATE><summary>Show Output</summary>
 
 \`\`\`
